4014af72c35df54d66c5e0259bd9d02af61955f2
patrikpapa <93653973+patrikpapa@users.noreply.github.com>
2023-01-17 13:25:16 +0100

    Add files via upload

---
+++ hajo.py
@@ -0,0 +1,16 @@
+hajo1=int(input('Adja meg a hajo sebességét!'))
+hajo2=int(input('Adja meg a hajo sebességét!'))
+
+if hajo1>hajo2:
+    print('Hajo1 gyorsabb mint hajo2')
+elif:
+    if hajo1<hajo2:
+    print('Hajo2 gyorsabb mint hajo1')
+else:
+    print('Egyenlo sebességük')
+    
+def valto (sebesseg):
+    return sebesseg*1.852
+hajo1=int(input('Adja meg a hajo sebességét!'))
+hajo2=int(input('Adja meg a hajo sebességét!'))
+print(valto(hajo1), valto(hajo2))
